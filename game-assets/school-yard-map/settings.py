698c0e4de178f38fb342898cec43317deada1faa
Flipleerr <102913083+Flipleerr@users.noreply.github.com>
2022-04-03 22:17:49 +0200

    python files for school map

---
+++ game-assets/school-yard-map/settings.py
@@ -0,0 +1,17 @@
+level_map = [
+'                                  XXXX',
+'                                  XXXX',
+'                                  XXXX',
+'                                  XXXX',
+'                                  XXXX',
+'                                  XXXX',
+'    P                             XXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+print(len(level_map))
